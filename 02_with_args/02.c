db266ade023de5f5ea7d001aa505239a644c3982
CristianLupascu <lupascugabrielcristian@yahoo.com>
2024-10-04 03:07:17 +0300

    with arguments

---
+++ 02_with_args/02.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    // argc - number of arguments
+    // argv - array of strings arguments
+    
+    printf("Hello %s\n", argv[1]);
+
+    return 0;
+}
